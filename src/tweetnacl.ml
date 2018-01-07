02efe6215fecad0939bfbe2c082f3a6c2e6ab7c0
Vincent Bernardoff <vb@luminar.eu.org>
2018-01-07 17:07:25 +0100

    add license, opam file

--- src/tweetnacl.ml
+++ src/tweetnacl.ml
@@ -1,3 +1,8 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2017 Vincent Bernardoff. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+  ---------------------------------------------------------------------------*)
+
 module Rand = struct
   external randombytes : Cstruct.buffer -> int -> unit =
     "ml_randombytes" [@@noalloc]
@@ -296,3 +301,19 @@ module Sign = struct
     | Sk cs -> Pk (Cstruct.sub cs 32 32)
     | Ek cs -> Pk (base_direct (Cstruct.sub cs 0 32))
 end
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2017 Vincent Bernardoff
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
